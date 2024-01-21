075fe6b6cd08f4cf80af857e2f722bd02b6ceb3e
Carlos Frias <carlos@peerwell.co>
2024-01-21 19:22:54 +0100

    feat: auth - protect routes
    Ref: https://auth0.com/docs/quickstart/webapp/nextjs/01-login

--- app/wallet/page.jsx
+++ app/wallet/page.jsx
@@ -1,7 +1,10 @@
-export default function Wallet() {
+import { getSession } from "@auth0/nextjs-auth0";
+
+export default async function Wallet() {
+  const { user } = await getSession();
   return (
     <div>
-      <p>Welcome to your wallet</p>
+      <p>Welcome to your wallet, {user.name}</p>
       <div>
         {/* TODO Link? */}
         <a href="/profile">Profile</a>
